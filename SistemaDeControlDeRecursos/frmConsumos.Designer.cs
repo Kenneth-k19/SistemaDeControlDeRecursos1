d339eb500fc7e172bd7d1340931d60e266a9bfec
Kenneth <kennethmendez702@gmail.com>
2024-11-08 23:15:13 -0600

    Se agregó la funcionalidad de scroll en los forms que faltaban.

--- SistemaDeControlDeRecursos/frmConsumos.Designer.cs
+++ SistemaDeControlDeRecursos/frmConsumos.Designer.cs
@@ -145,6 +145,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(800, 619);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnInsertar);
--- SistemaDeControlDeRecursos/frmFamilia.Designer.cs
+++ SistemaDeControlDeRecursos/frmFamilia.Designer.cs
@@ -144,6 +144,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(800, 532);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.label2);
