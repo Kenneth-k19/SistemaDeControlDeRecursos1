1b69627e12f6ea0a4a5f60738d75c123b7e016ec
Daniel Madrid <danielmadrid@unah.hn>
2024-11-09 19:14:06 -0600

    cambios en el diseño de la plantilla, corrigiendo margenes

--- SistemaDeControlDeRecursos/frmLogin.Designer.cs
+++ SistemaDeControlDeRecursos/frmLogin.Designer.cs
@@ -145,6 +145,7 @@
             // 
             // frmLogin
             // 
+            this.AcceptButton = this.btnIngresar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(351, 494);
